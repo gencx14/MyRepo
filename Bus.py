72f4282f14041df1bbe961f30c44a4c8911d95e4
Nicholas Genco <neg47@pitt.edu>
2023-01-18 20:18:08 -0500

    Bus Class set and get functions added

--- Bus.py
+++ Bus.py
@@ -4,3 +4,10 @@ class Bus:
     def __init__(self, name):
         Bus.busCount += 1
         Bus.name = name
+    def getBusCount(self):
+        return self.busCount
+    def getName(self):
+        return self.name
+    def setName(self, name):
+        Bus.name = name
+

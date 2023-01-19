3bf4ac35681dd72b216b83b1492e05bcb1281394
Nicholas Genco <neg47@pitt.edu>
2023-01-18 20:02:41 -0500

    Load Function has get and set function

--- Load.py
+++ Load.py
@@ -6,4 +6,21 @@ class Load:
         self.V_rated = voltageRating
 
     #set functions
+    def setName(self, name):
+        self.name = name
+    def setbus1(self, BusObject):
+        self.bus1 = BusObject
+    def setPower(self, power):
+        self.P_rated = power
+    def setvoltage(self, voltageRating):
+        self.V_rated = voltageRating
     #get functions
+    def getName(self):
+        return self.name
+    def getbus1(self):
+        return self.bus1
+
+    def getRatedPower(self):
+        return self.P_rated
+    def getRatedVoltage(self):
+        return self.V_rated

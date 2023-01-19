fa69aaa54e8a7cc8f34f4c377a403149f9e428be
Nicholas Genco <neg47@pitt.edu>
2023-01-18 20:25:54 -0500

    Voltage Source Class added

---
+++ VoltageSource.py
@@ -0,0 +1,19 @@
+class VoltageSource:
+    def __int__(self, name, bus1, voltage):
+        self.name = name
+        self.bus1 = bus1
+        self.voltage = voltage
+
+    def setName(self, name):
+        self.name = name
+    def setBus1(self, bus1):
+        self.bus1 = bus1
+    def setVoltage(self, voltage):
+        self.voltage = voltage
+
+    def getName(self):
+        return self.name
+    def getBus1(self):
+        return self.bus1
+    def getVolage(self):
+        return self.voltage

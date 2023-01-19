17049d7a909024822059b1488b90ee2642b97744
Nicholas Genco <neg47@pitt.edu>
2023-01-18 20:15:21 -0500

    Resistor Class added

---
+++ Resistor.py
@@ -0,0 +1,23 @@
+class Resistor:
+    def __int__(self, name, bus1, bus2, ohms):
+        self.name = name
+        self.bus1 = bus1
+        self.bus2 = bus2
+        self.resistance = ohms
+
+    def setName(self, name):
+        self.name = name
+    def setBus1(self, bus1):
+        self.bus1 = bus1
+    def setBus2(self, bus2):
+        self.bus2 = bus2
+    def setResistance(self, ohms):
+        self.resistance = ohms
+    def getName(self):
+        return self.name
+    def getBus1(self):
+        return self.bus1
+    def getBus2(self):
+        return self.bus2
+    def getResistance(self):
+        return self.resistance

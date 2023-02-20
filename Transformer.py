50be83ffca649dfa39b437d79c4bc7ca5502727d
Nicholas Genco <neg47@pitt.edu>
2023-02-20 00:49:06 -0500

    Transformer class further updated and completed for milestone 1 (i think)

--- Transformer.py
+++ Transformer.py
@@ -14,13 +14,13 @@ class Transformer:
         self.vBaseSecondary = vSecondary
         self.zPct = zPctTransformer
         self.xrRatio = xrRatio
-        self.zPUphasor = [self.makeZpuPhasor(self.zPct, self.xrRatio)] #zPUphasor[0] is magnitude while [1] is phase
+        self.zPUphasor = [self.makeZpuPhasor(self.zPct, self.xrRatio, self.vPrimary, self.vSecondary, sRated)] #zPUphasor[0] is magnitude while [1] is phase
         self.zPuRect = cmath.rect(*self.zPUphasor)  #has a list be passed in as multiple argurments to a function... so this is mag and phase
         self.txresistancePU = self.zPuRect.real
         self.txreactancePu = self.zPuRect.imag
 
-    def makeZpuPhasor(self, zPCT, xrRatio, vlow, vhigh, srated):
-            zTransformerPU = (zPCT / 100) * cmath.exp(cmath.atan(xrRatio) * 1j) * ((vlow**2/srated)/(vhigh**2/100))
+    def makeZpuPhasor(self, zPCT, xrRatio, vprim, srated): #is it vlow or v primary????
+            zTransformerPU = (zPCT / 100) * cmath.exp(cmath.atan(xrRatio) * 1j) * ((vprim**2/srated)/(vprim**2/100))
             return cmath.polar(zTransformerPU)
 
 

9e81cfaba0194cd9c81d93a190b02933bd0c6eb7
Nicholas Genco <neg47@pitt.edu>
2023-02-20 02:15:30 -0500

    Updated TransmissionLineData.py to do all calculations for the line

--- TransmissionLineData.py
+++ TransmissionLineData.py
@@ -14,8 +14,10 @@ class TransmissionLineData:
         self.d = bundleDisance #distance between conductors in a bundle
         self.gmr = gmr #gmr in feet
         self.radius = (diameter/2)/12 #conductor radius in feet
-        self.ohmsPerMeter = rAC/(bundleSize) #R = R1/bundlesize      ********THIS MAY NOT BE THE RIGHT UNITS... CHeck if it is per meter or per mile
+        self.ohmsPerMile = rAC/(bundleSize) #R = R1/bundlesize      ******THIS MAY NOT BE THE RIGHT UNITS... CHeck if it is per meter or per mile
+        self.rPrime = self.ohmsPerMile / 1609 #ohms per meter R'
         self.deq = self.geom.getdeq()
+        self.w = 2 * cmath.pi * 60                                 #    *******Figure out where to input frequency*******
 
         if bundleSize == 1:
             self.dsl = self.gmr
@@ -33,6 +35,10 @@ class TransmissionLineData:
             print("This program can only handle bundleSizes of 4 or less. Please adjust your input")
             sys.exit() #ends the program
 
-        self.cpm = 2*cmath.pi*(8.854*10**(-12))/cmath.log(self.deq / self.dsc) #units of Ferads per meter     ***** Check your units
-        self.lpm = (2*10**(-7)) * cmath.log(self.deq / self.dsl) #inductance per meter H/m
+        self.cPrime = 2*cmath.pi*(8.854*10**(-12))/cmath.log(self.deq / self.dsc) #units of Ferads per meter  C'   ***** Check your units
+        self.lPrime = (2*10**(-7)) * cmath.log(self.deq / self.dsl) #inductance per meter H/m L'
+        self.cpm = self.cPrime * 1609 #F / mile
+        self.lpm = self.lPrime * 1609 #H / mile
+        self.zseries = self.ohmsPerMile + 1j * self.w * self.lpm #Zline per mile
+        self.yshunt = 1j * self.w * 1j * self.w * self.cpm #Yshunt per mile
 

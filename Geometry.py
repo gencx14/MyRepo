3f0898e0e54cb8dc9d9986d9e10e0e432888a19e
Nicholas Genco <neg47@pitt.edu>
2023-03-16 00:16:02 -0400

    nearing completion of y bus

--- Geometry.py
+++ Geometry.py
@@ -1,7 +1,7 @@
 import cmath
 class Geometry:
 
-    def __int__(self, name: str, ax, ay, bx, by, cx, cy):
+    def __init__(self, name: str, ax, ay, bx, by, cx, cy):
         self.dab = cmath.sqrt((ax-bx)**2 - (ay - by)**2)
         self.dbc = cmath.sqrt((bx-cx)**2 - (by - cy)**2)
         self.dca = cmath.sqrt((cx - ax) ** 2 - (cy - ay) ** 2)
